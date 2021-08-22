d71be19248b72d6fb516292ea308f75ad28af4b2
Nuel-C <eniemmanuel.c@gmail.com>
2021-08-22 10:25:04 +0100

    debuged connection error

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ global.username = ''
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "*",
       methods: ["GET", "POST", "PUT", "DELETE"]
     }
   });
